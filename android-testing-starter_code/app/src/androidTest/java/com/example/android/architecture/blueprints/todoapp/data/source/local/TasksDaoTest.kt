8b40a52faa4796451342dc533d4e5603ceed7773
Ashkeyev Ali <ashkeyevli@gmail.com>
2020-11-23 00:28:50 +0600

    Test part3:Survey of Testing Topics

---
+++ android-testing-starter_code/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksDaoTest.kt
@@ -0,0 +1,81 @@
+package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import org.hamcrest.MatcherAssert.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class TasksDaoTest {
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: ToDoDatabase
+
+
+    @Before
+    fun initDb() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+                getApplicationContext(),
+                ToDoDatabase::class.java
+        ).allowMainThreadQueries().build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+
+    @Test
+    fun insertTaskAndGetById() = runBlockingTest {
+        // GIVEN - Insert a task.
+        val task = Task("title", "description")
+        database.taskDao().insertTask(task)
+
+        // WHEN - Get the task by id from the database.
+        val loaded = database.taskDao().getTaskById(task.id)
+
+        // THEN - The loaded data contains the expected values.
+        assertThat<Task>(loaded as Task, notNullValue())
+        assertThat(loaded.id, `is`(task.id))
+        assertThat(loaded.title, `is`(task.title))
+        assertThat(loaded.description, `is`(task.description))
+        assertThat(loaded.isCompleted, `is`(task.isCompleted))
+    }
+
+    @Test
+    fun updateTaskAndGetById() = runBlockingTest {
+        // 1. Insert a task into the DAO.
+        val task = Task("titleUpdate", "descriptionUpdate")
+        database.taskDao().insertTask(task)
+        // 2. Update the task by creating a new task with the same ID but different attributes.
+        val task1 = Task("titleUpdatedTask", "descriptionUpdatedTask", true, task.id)
+        database.taskDao().updateTask(task1)
+
+        val loaded = database.taskDao().getTaskById(task1.id)
+
+        // 3. Check that when you get the task by its ID, it has the updated values.
+
+        assertThat(loaded?.id, `is`(task.id))
+        assertThat(loaded?.title, `is`("titleUpdatedTask"))
+        assertThat(loaded?.description, `is`("descriptionUpdatedTask"))
+        assertThat(loaded?.isCompleted, `is`(true))
+    }
+
+}
--- android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,24 +3,37 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.R
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.hamcrest.Matchers.*
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
+@ExperimentalCoroutinesApi
 class TasksViewModelTest{
     private lateinit var tasksRepository: FakeTestRepository
     private lateinit var tasksViewModel: TasksViewModel
 
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+
 
     @Before
     fun setUpViewModel() {
@@ -31,6 +44,9 @@ class TasksViewModelTest{
         tasksRepository.addTasks(task1, task2, task3);
         tasksViewModel = TasksViewModel(tasksRepository)
     }
+
+
+
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -43,5 +59,19 @@ class TasksViewModelTest{
     fun setFilterAllTasks_tasksAddViewVisible() {
         tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
         assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
+    }
+
+    @Test
+    fun completeTask_dataAndSnackbarUpdated() {
+        val task = Task("Title", "Description")
+        tasksRepository.addTasks(task)
+
+        tasksViewModel.completeTask(task, true)
+
+        // Verify the task is completed.
+        assertThat(tasksRepository.tasksServiceData[task.id]?.isCompleted, `is`(true))
+
+        val snackbarText: Event<Int> =  tasksViewModel.snackbarText.getOrAwaitValue()
+        assertThat(snackbarText.getContentIfNotHandled(), `is`(R.string.task_marked_complete))
     }
 }
